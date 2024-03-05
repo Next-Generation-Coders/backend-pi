a3854c02332831434f76e3e267d2386d08e91283
MeryemMriri <meriem.mriri@esprit.tn>
2024-03-05 17:49:07 +0100

    GoalsTrackingFinal

--- models/Match.js
+++ models/Match.js
@@ -3,38 +3,36 @@ const Team = require('./Team');
 const Result = require('./Result');
 const Tournament = require('./Tournament');
 
-const Schema = mongoose.Schema
+const matchSchema = new mongoose.Schema({
+  team1: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Team',
+    required: true
+  },
+  team2: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Team',
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now 
+  },startDay : Number,
+  startMonth : Number,
+  startYear : Number,
+  endDay : Number,
+  endMonth : Number,
+  endYear : Number,
+  result : {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'MatchResult'
+  },
+  tournament: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Tournament'
+  }   
+});
 
-const Match = new Schema({
-    
-    team1: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Team',
-        required: true
-      },
-      team2: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Team',
-        required: true
-      },
-      date: {
-        type: Date,
-        default: Date.now 
-      },
-    startDay : Number,
-    startMonth : Number,
-    startYear : Number,
-    endDay : Number,
-    endMonth : Number,
-    endYear : Number,
-    result : {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'MatchResult'
-    },
-    tournament: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Tournament'
-    }    
-})
+const Match = mongoose.model('Match', matchSchema);
 
-module.exports = mongoose.model('match',Match);
+module.exports = Match;
