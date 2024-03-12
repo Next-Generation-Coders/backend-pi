c7e1fdebebf7e865f63aced19f77ade85350fa40
MeryemMriri <meriem.mriri@esprit.tn>
2024-03-12 21:33:22 +0100

    Result Tracking

--- app.js
+++ app.js
@@ -52,13 +52,118 @@ io.on("connection", (socket) => {
     } catch (error) {
       console.error('Error:', error.message);
     }
-  });
+  } );
+  socket.on('red', async ({ team }) => {
+    try {
+      // Update match data in the database based on the team that scored
+      const result = await Result.findOne().populate('match');
+      if (!result) {
+        throw new Error('Match not found');
+      }
+
+      if (team === 'team1') { 
+        console.log("team1 red")
+        result.team1Red++;
+      } else if (team === 'team2') {
+        console.log("team2 red")
+
+        result.team2Red++;
+      }
+
+      await result.save();
+
+      // Emit scoreUpdate event to all connected clients with updated score information
+      io.emit('redUpdate', { team1Red: result.team1Red, team2Red: result.team2Red });
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+  );
+  socket.on('yellow', async ({ team }) => {
+    try {
+      // Update match data in the database based on the team that scored
+      const result = await Result.findOne().populate('match');
+      if (!result) {
+        throw new Error('Match not found');
+      }
+
+      if (team === 'team1') { 
+        console.log("team1 yellow")
+        result.team1Yellow++;
+      } else if (team === 'team2') {
+        console.log("team2 yellow")
+
+        result.team2Yellow++;
+      }
+
+      await result.save();
+
+      // Emit scoreUpdate event to all connected clients with updated score information
+      io.emit('yellowUpdate', { team1Yellow: result.team1Yellow, team2Yellow: result.team2Yellow });
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+  );
+  socket.on('corners', async ({ team }) => {
+    try {
+      // Update match data in the database based on the team that scored
+      const result = await Result.findOne().populate('match');
+      if (!result) {
+        throw new Error('Match not found');
+      }
+
+      if (team === 'team1') { 
+        console.log("team1 corner")
+        result.team1Corners++;
+      } else if (team === 'team2') {
+        console.log("team2 corner")
+
+        result.team2Corners++;
+      }
+
+      await result.save();
+
+      // Emit scoreUpdate event to all connected clients with updated score information
+      io.emit('cornersUpdate', { team1Corners: result.team1Corners, team2Corners: result.team2Corners });
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+  );
+  socket.on('offsides', async ({ team }) => {
+    try {
+      // Update match data in the database based on the team that scored
+      const result = await Result.findOne().populate('match');
+      if (!result) {
+        throw new Error('Match not found');
+      }
+
+      if (team === 'team1') { 
+        console.log("team1 offside")
+        result.team1Offsides++;
+      } else if (team === 'team2') {
+        console.log("team2 offside")
+
+        result.team2Offsides++;
+      }
+
+      await result.save();
+
+      // Emit scoreUpdate event to all connected clients with updated score information
+      io.emit('offsidesUpdate', { team1Offsides: result.team1Offsides, team2Offsides: result.team2Offsides });
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  }
+  );
+ 
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
 
-server.listen(3001,console.log("server is running"))
+server.listen(3000,console.log("server is running"))
  
 module.exports =app
